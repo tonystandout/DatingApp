a14f9d0f8f8734a36b729edbdd76deb33629884f
tonystandout <tony.standout@gmail.com>
2021-11-29 16:06:12 +0530

    End of section 11

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -45,6 +45,7 @@ namespace API.Controllers
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
             var user =await _context.Users
+            .Include(p=> p.Photos)
             .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
             if(user==null) return Unauthorized("Invalid username");
             using var hmac =new HMACSHA512(user.PasswordSalt);
@@ -56,7 +57,8 @@ namespace API.Controllers
             return new UserDto
             {
             Username=user.UserName,
-            Token=_tokenService.CreateToken(user)
+            Token=_tokenService.CreateToken(user),
+            PhotoUrl=user.Photos.FirstOrDefault(x=>x.IsMain)?.Url
             };
         }
         private async Task<bool> UserExists(string username)
