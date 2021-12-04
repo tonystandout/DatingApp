ac2bf545464e52b13b04e680bcb145637b7c722f
tonystandout <tony.standout@gmail.com>
2021-12-04 21:17:46 +0530

    End of section 13

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -25,7 +25,8 @@ namespace API.Services
         {
             var claims = new List<Claim>
             {
-                new Claim(JwtRegisteredClaimNames.NameId,user.UserName)
+                new Claim(JwtRegisteredClaimNames.NameId,user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName,user.UserName)
             };
             var creds=new SigningCredentials(_Key,SecurityAlgorithms.HmacSha512Signature);
             var tokenDescriptor = new SecurityTokenDescriptor
